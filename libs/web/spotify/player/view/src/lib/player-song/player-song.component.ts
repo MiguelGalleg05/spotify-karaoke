68d860f1427600b2ae65e98f5904761f32548058
Artur Bauer <bauerart1@gmail.com>
2021-04-23 18:40:52 +0200

    feat(spotify): :sparkles: Add current playing data  (#17)
    * feat(spotify): :sparkles: Add current playing data
    
    * ci(ci): :package: Add tailwind packages

--- libs/web/spotify/player/view/src/lib/player-song/player-song.component.ts
+++ libs/web/spotify/player/view/src/lib/player-song/player-song.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
 
+import { PlayerStore } from '@artur-ba/shared/service';
 @Component({
   selector: 'artur-ba-player-song',
   templateUrl: './player-song.component.html',
   styleUrls: ['./player-song.component.scss'],
 })
-export class PlayerSongComponent {}
+export class PlayerSongComponent {
+  track: Spotify.Track;
+
+  constructor(protected playerStore: PlayerStore) {
+    this.playerStore.currentTrack$.subscribe((track) => {
+      if (this.track !== track) {
+        this.track = track;
+      }
+    });
+  }
+
+  get image64Url(): string {
+    const img = this.track.album.images.filter((image) => image.height == 64);
+    if (img[0]) {
+      return img[0].url;
+    }
+  }
+}
