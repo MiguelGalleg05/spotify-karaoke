e1a4606db5942038ee32bd7304126f1d369be734
Artur Bauer <bauerart1@gmail.com>
2021-04-25 21:59:09 +0200

    feat(spotify): :sparkles: add basic menu in sidebar (#23)
    * feat(shared): :lipstick: Add and configure setup for dark mode
    
    * feat: :lipstick: Set colors
    
    * fix(spotify): :rotating_light: Fix lining warniing aboud end/start in flex
    
    * fix(spotify): :lipstick: Remove remaining tailwindcss
    
    * Add artists on song view
    
    * feat: :lipstick: Set up dark/light mode for the app
    
    * fix(spotify): :lipstick: Fix flier background
    
    * docs: :memo: Change project name in package.json
    
    * feat: :lipstick: Min color variables
    
    * build: :green_heart: Fix redirects on netlify
    
    * feat: :lipstick: add generated theme
    
    * Add logo in selected color scheme
    
    * feat: :chart_with_upwards_trend: Add google analytics
    
    * feat(spotify): :white_check_mark: Fix coma values. Add tests for shared spotify pipes
    
    * fix(spotify): :bug: Improve work of uri cutter pipe
    
    * fix(shared): :rotating_light: Fix linting sort order
    
    * feat(spotify): :sparkles: add basic menu in sidebar

--- libs/web/spotify/sidebar/view/src/lib/spotify-sidebar-view.module.ts
+++ libs/web/spotify/sidebar/view/src/lib/spotify-sidebar-view.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild([])],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([]),
+    MatButtonModule,
+    MatIconModule,
+  ],
   declarations: [SidebarComponent],
   exports: [SidebarComponent],
 })
